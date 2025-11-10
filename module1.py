deec71b00a749ea4d7433e7ffbb700637c37224a
Thirupathi Reddaveni <thirudarling7834@gmail.com>
2025-11-10 15:44:36 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def operators():
+    ''' This function tells the all operators in python'''
+    print('Arithmetic Operators')
+    print('Relational Operators')
+    print('Membership Operators')
+    print('Logical Operators')
+    print('Bitwsise Operators')
+    print('Identity Operators')
+    print('Assignment Operators')
+
+def datatypes():
+    ''' This function tells the all data types in python'''
+    print('Integer')
+    print('Float')
+    print('Complex')
+    print('Boolean')
+    print('string')
+
+def datastructers():
+    ''' This function tells the data structers in python '''
+    print('List')
+    print('Tuple')
+    print('Set')
+    print('Dictionary')
+    print('Frozen set')
+
+def conditionalstatements():
+    '''This function tells the conditional statements in python'''
+    print('if condition')
+    print('if-else condition')
+    print('if-elif-else condition')
+    print('Neseted if condition')
+
+def loops():
+    ''' This function tells the loops in python'''
+    print('for loop')
+    print('while loop')
+
+def loopcontrolstatements():
+    ''' This function tells the loop control statements in python'''
+    print('break')
+    print('continue')
+    print('pass')
+
+def functions():
+    ''' This function tells the function in python '''
+    print('Built in functions')
+    print('Custom user fucntions')
+
+def arguments():
+    ''' This function tells the types of arguments in python'''
+    print('Positional arguments')
+    print('Keyword argumrnts')
+    print('Default arguments')
+    print('Variable length')
+
+def variables():
+    ''' This function tells the variables in python'''
+    print('Local varibales')
+    print('Global variables')
+
+def exceptionhandling():
+    ''' This function tells the exception handling ways in python'''
+    print('try')
+    print('else')
+    print('except')
+    print('finallly')
+
+def errors():
+    ''' This function tells the error types in python'''
+    print('Programmers errors')
+    print('Run time errors')
+
+def modules():
+    ''' This function tells the modules in python'''
+    print('Built in moodule')
+    print('User defined module')
+    print('Third party module')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
